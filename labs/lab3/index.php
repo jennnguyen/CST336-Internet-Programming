c5077b21b335294195d0623ed98760a4573083dc
Jennifer Nguyen <jennnguyen@csumb.edu>
2017-09-25 06:04:48 +0000

    added lab3

---
+++ labs/lab3/index.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    
+    include 'inc/functions.php'
+    
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Silver Jack </title>
+    </head>
+    <body>
+        <?php
+        
+            play();
+            
+        ?>
+    </body>
+</html>
